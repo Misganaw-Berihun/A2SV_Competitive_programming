d0ec4c76aee130d3da8e8c000007897daef4bb6a
Misganaw Berihun <MisganawBMB@gmail.com>
2021-12-14 16:48:34 +0300

    day_3

---
+++ Week1/1859_Sorting_the_Sentence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String sortSentence(String s) {
+        String [] strArr = s.split(" ");
+        String [] sortedArr = new String[strArr.length];
+
+        int wPos;
+        int idx;
+        for(int i = 0; i < strArr.length; i++){
+          idx = strArr[i].length() - 1; //find index of digit
+          wPos = strArr[i].charAt(idx) - '0' - 1;//convert the char index to int then subtract 1
+
+          //store the value of substring without digit at sortedArr at index one minus postion number
+          sortedArr[wPos] = strArr[i].substring(0,idx);
+        }
+
+        String sortedSentence = "";
+        for(String str: sortedArr){
+            sortedSentence += ( str + " ");
+        }
+        idx = sortSentence.length() - 1;
+        sortedSentence = sortedSentence.substring(0, idx); // removing the last space character
+
+        return sortedSentence;
+    }
+}
